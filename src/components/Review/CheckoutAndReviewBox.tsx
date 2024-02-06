fb3d0d37d7e703deef1e1efc365f3e3cae1a1f8b
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 14:38:54 +0200

    feat(review): Added CheckoutAndReviewBox.tsx component

---
+++ src/components/Review/CheckoutAndReviewBox.tsx
@@ -0,0 +1,10 @@
+const CheckoutAndReview = () =>{
+    
+    return (
+        <div className="container">
+            <h3 className="fw-bold text-center text-white">Залишити відгук</h3>
+        </div>
+    );
+}
+
+export default CheckoutAndReview;
