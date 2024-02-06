27b1f7169a05e9c7d21ae030f6765ec97e5e8c02
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 14:43:33 +0200

    feat(search): Deleted all enum genres and added new offcanvas with genres that gets from api

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,24 +1,17 @@
 import { useState, useEffect } from "react";
-import MovieModel from "../../models/MovieModel";
+import GenreModel from "../../models/GenreModel";
 import SearchMovie from "../../components/Search/SearchMovie";
 import Pagination from "../../components/Universal/Utils/Pagination";
 import CustomError from "../../components/Universal/Utils/CustomError";
 import SpinnerLoading from "../../components/Universal/Utils/SpinnerLoading";
+import { BASE_URL } from "../../constants/BASE_URL";
+import Movie from "../../models/MovieModel";
 
 const Search = () => {
-  const movieGenres: string[] = [
-    "ALL",
-    "COMEDY",
-    "FANTASY",
-    "HORRORS",
-    "ACTION_FILM",
-    "MELODRAMA",
-    "ADVENTURES",
-    "MYSTICISM",
-    "DETECTIVE",
-    "ANIMATION",
-  ];
-  const [movies, setMovies] = useState<MovieModel[]>([]);
+  const [movieGenres, setMovieGenres] = useState<GenreModel[]>([]);
+  const [selectedGenres, setSelectedGenres] = useState<number[]>([]);
+
+  const [movies, setMovies] = useState<Movie[]>([]);
   const [httpError, setHttpError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   //Pagination realisation
@@ -29,8 +22,6 @@ const Search = () => {
   //Search by title
   const [search, setSearch] = useState("");
   const [searchUrl, setSearchUrl] = useState("");
-  //Seacrh by genre
-  const [genreSelection, setGenreSelection] = useState("Movie genre");
   const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
   };
@@ -38,7 +29,7 @@ const Search = () => {
   //
   useEffect(() => {
     const fetchMovies = async () => {
-      const baseUrl: string = "http://localhost:8080/api/movie";
+      const baseUrl: string = BASE_URL + "/movie";
       const url =
         searchUrl === ""
           ? `${baseUrl}?page=${currentPage - 1}&size=${moviesPerPage}`
@@ -46,6 +37,7 @@ const Search = () => {
               "<pageNumber>",
               `${currentPage - 1}`
             )}`;
+      console.log(url);
       const response = await fetch(url);
       const responseJson = await response.json();
       if (!response.ok) {
@@ -57,12 +49,13 @@ const Search = () => {
       setTotalPages(responseJson.totalPages);
       setTotalAmountOfMovies(responseJson.totalElements);
 
-      const loadedMovies: MovieModel[] = [];
+      const loadedMovies: Movie[] = [];
       for (const key in responseData) {
         loadedMovies.push(
-          new MovieModel(
+          new Movie(
             responseData[key].id,
             responseData[key].title,
+            responseData[key].requiredAge,
             responseData[key].year,
             responseData[key].production,
             responseData[key].genres,
@@ -70,7 +63,9 @@ const Search = () => {
             responseData[key].duration,
             responseData[key].language,
             responseData[key].description,
-            `${baseUrl}/${responseData[key].id}/image`
+            responseData[key].trailerUrl,
+            responseData[key].videoUrl,
+            responseData[key].imageUrls
           )
         );
       }
@@ -84,30 +79,53 @@ const Search = () => {
     window.scrollTo(0, 0);
   }, [currentPage, searchUrl]);
 
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const response = await fetch(BASE_URL + "/genre?sort=id");
+      const responseJson = await response.json();
+      if (!response.ok) {
+        setHttpError(responseJson);
+        throw new Error("Something went wrong!");
+      }
+      const responseData = responseJson.content;
+
+      const loadedGenres: GenreModel[] = [];
+      for (const key in responseData) {
+        loadedGenres.push(
+          new GenreModel(responseData[key].id, responseData[key].genreName)
+        );
+      }
+      setMovieGenres(loadedGenres);
+    };
+    fetchGenres().catch((error: any) => {
+      setIsLoading(false);
+      console.log(error.message);
+    });
+  }, []);
+
   const searchHandleChange = () => {
     setCurrentPage(1);
     search === ""
-      ? (setSearchUrl(""), setGenreSelection("ALL"))
+      ? setSearchUrl("")
       : setSearchUrl(
-          `/title?title=${search}&page=<pageNumber>&size=${moviesPerPage}`
+          `?title=${search}&page=<pageNumber>&size=${moviesPerPage}`
         );
   };
 
-  const genreHandleChange = (genre: string) => {
-    setGenreSelection(genre);
+  const handleGenreChange = (genreId: number) => {
+    const updatedGenres = selectedGenres.includes(genreId)
+      ? selectedGenres.filter((id) => id !== genreId)
+      : [...selectedGenres, genreId];
+
+    setSelectedGenres(updatedGenres);
     setCurrentPage(1);
-    if (genre !== "ALL") {
-      const selectedGenreMovies = movieGenres.filter(
-        (movieGenre) => movieGenre === genre
-      );
-      if (selectedGenreMovies.length > 0) {
-        setSearchUrl(
-          `/genre?genre=${genre}&page=<pageNumber>&size=${moviesPerPage}`
-        );
-      }
-    } else {
-      setSearchUrl(`?page=<pageNumber>&size=${moviesPerPage}`);
-    }
+
+    const genreIdsString = updatedGenres.join(",");
+    setSearchUrl(
+      genreIdsString
+        ? `?genreIds=${genreIdsString}&page=<pageNumber>&size=${moviesPerPage}`
+        : ""
+    );
   };
 
   if (isLoading) {
@@ -127,55 +145,79 @@ const Search = () => {
               <input
                 className="form-control me-2"
                 type="search"
-                placeholder="Search"
+                placeholder="Введіть назву фільму"
                 aria-labelledby="Search"
                 onChange={(e) => setSearch(e.target.value)}
               />
               <button
                 className="btn btn-outline-success"
-                onClick={() => {
-                  searchHandleChange();
-                }}
+                onClick={searchHandleChange}
               >
-                Search
+                Пошук
               </button>
-              <div className="mx-2 d-none d-lg-block">
-                <div className="dropdown justify-content-center">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    {genreSelection}
-                  </button>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton1"
-                  >
-                    {movieGenres.map((movieGenre) => (
-                      <li
-                        key={movieGenres.indexOf(movieGenre)}
-                        onClick={() => {
-                          genreHandleChange(movieGenre);
-                        }}
-                      >
-                        <a className="dropdown-item" href="#">
-                          {movieGenre}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
+              <button
+                className="btn btn-outline-primary ms-2"
+                type="button"
+                data-bs-toggle="offcanvas"
+                data-bs-target="#staticBackdrop"
+                aria-controls="staticBackdrop"
+              >
+                Жанри
+              </button>
             </div>
           </div>
         </div>
-        <div className="mt-3"></div>
-        {movies.map((movie) => (
-          <SearchMovie movie={movie} key={movie.id} />
-        ))}
+
+        <div
+          className="offcanvas offcanvas-start"
+          data-bs-backdrop="static"
+          id="staticBackdrop"
+          aria-labelledby="staticBackdropLabel"
+          style={{backgroundColor: "#221f1f"}}
+        >
+          <div className="offcanvas-header d-flex justify-content-end">
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="offcanvas-body">
+          <div>
+          <h5 className="fw-bold text-white text-center" id="staticBackdropLabel">
+              Список жанрів
+            </h5>
+            <div className="d-flex flex-column align-items-start">
+              {movieGenres.map((genre) => (
+                <div key={genre.id} className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="checkbox"
+                    id={`genre-${genre.id}`}
+                    checked={selectedGenres.includes(genre.id)}
+                    onChange={() => handleGenreChange(genre.id)}
+                  />
+                  <label
+                    className="form-check-label text-white"
+                    htmlFor={`genre-${genre.id}`}
+                  >
+                    {genre.genreName}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+          </div>
+        </div>
+
+        <div className="row mt-3">
+          <div className="col-md-12">
+            {movies.map((movie) => (
+              <SearchMovie movie={movie} key={movie.id} />
+            ))}
+          </div>
+        </div>
       </div>
       <Pagination
         currentPage={currentPage}
