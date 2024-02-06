8c0009aea548af09792d6676d8ceb9011646abc7
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 14:45:45 +0200

    feat(movie-checkout): Rewritten MovieCheckout.tsx

--- src/components/MovieCheckout/MovieCheckout.tsx
+++ src/components/MovieCheckout/MovieCheckout.tsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
-import MovieModel from "../../models/MovieModel";
 import CustomError from "../Universal/Utils/CustomError";
 import SpinnerLoading from "../Universal/Utils/SpinnerLoading";
+import { BASE_URL } from "../../constants/BASE_URL";
+import { parseDuration } from "../../constants/DurationParser";
+import Video from "../Universal/Video/Video";
+import StarReview from "../Universal/Star/StarReview";
+import Movie from "../../models/MovieModel";
+import Reviews from "../Review/Reviews";
+import CheckoutAndReview from "../Review/CheckoutAndReviewBox";
+
 const MovieCheckout = () => {
-  const [movie, setMovie] = useState<MovieModel>();
+  const [movie, setMovie] = useState<Movie>();
   const [httpError, setHttpError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const movieId = window.location.pathname.split("/")[2];
+  let isUserAuthenticated = true;
 
   useEffect(() => {
     const fetchMovie = async () => {
-      const baseUrl = "http://localhost:8080/api/movie";
+      const baseUrl = BASE_URL + "/movie";
       const url = `${baseUrl}/${movieId}`;
       const response = await fetch(url);
 
@@ -19,22 +27,32 @@ const MovieCheckout = () => {
         setHttpError(responseJson);
         throw new Error("Something went wrong!");
       }
-      const loadedMovie: MovieModel = new MovieModel(
+
+      const jsonDuration = parseDuration(responseJson.duration);
+      const duration = {
+        hours: Math.floor(jsonDuration / 60),
+        minutes: jsonDuration % 60,
+      };
+
+      const loadedMovie: Movie = new Movie(
         responseJson.id,
         responseJson.title,
+        responseJson.requiredAge,
         responseJson.year,
-        responseJson.production,
+        responseJson.producingCountries,
         responseJson.genres,
-        responseJson.director,
-        responseJson.duration,
+        responseJson.directors,
+        duration.hours + " год. " + duration.minutes + " хв.",
         responseJson.language,
         responseJson.description,
-        `${baseUrl}/${responseJson.id}/image`
+        responseJson.trailerUrl,
+        responseJson.videoUrl,
+        responseJson.imageUrls
       );
 
       setMovie(loadedMovie);
       setIsLoading(false);
-      console.log(loadedMovie);
+      localStorage.setItem("movieId", movieId);
     };
     fetchMovie().catch((error: any) => {
       setIsLoading(false);
@@ -54,12 +72,12 @@ const MovieCheckout = () => {
     <div>
       <div className="container p-0 d-none d-lg-block">
         <div className="row mt-5">
-          <div className="col-sm-2 col-md-2 mx-5">
-            {movie?.imgUrl ? (
+          <div className="col-sm-2 col-md-2 mx-5 d-flex flex-column align-items-center text-center">
+            {movie?.imageUrls[0] ? (
               <img
-                src={movie?.imgUrl}
+                src={movie?.imageUrls[0]}
                 alt={movie?.title}
-                className="rounded position-fixed"
+                className="rounded"
               />
             ) : (
               <img
@@ -68,18 +86,44 @@ const MovieCheckout = () => {
                 className="rounded"
               />
             )}
+
+            {movie?.trailerUrl && (
+              <a
+                href={movie?.trailerUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button
+                  type="button"
+                  className="btn btn-dark mt-3 fw-bold mx-auto"
+                >
+                  Дивитись трейлер
+                </button>
+              </a>
+            )}
           </div>
-          <div className="col-4 col-md-4 mx-5 text-white">
+          <div className="col-4 col-md-6 mx-5 text-white">
             <div className="ml-2 w-100">
               <h2 className="fw-bold">{movie?.title}</h2>
               <ul className="list-unstyled">
+                <li className="d-flex mb-2">
+                  <p className="me-2 fw-bold">Вік:</p>
+                  <p>{movie?.requiredAge + "+"}</p>
+                </li>
                 <li className="d-flex mb-2">
                   <p className="me-2 fw-bold">Рік:</p>
                   <p>{movie?.year}</p>
                 </li>
                 <li className="d-flex mb-2">
                   <p className="me-2 fw-bold">Режисер:</p>
-                  <p>{movie?.director}</p>
+                  <p>
+                    {movie?.directors
+                      ?.map(
+                        (director) =>
+                          `${director.firstName} ${director.lastName}`
+                      )
+                      .join(", ")}
+                  </p>
                 </li>
                 <li className="d-flex mb-2">
                   <p className="me-2 fw-bold">Мова:</p>
@@ -88,38 +132,49 @@ const MovieCheckout = () => {
                 <li className="d-flex mb-2">
                   <p className="me-2 fw-bold">Жанр:</p>
                   <div>
-                    {movie?.genres.map((genre, index) => (
+                    {movie?.genres?.map((genre, index) => (
                       <span key={index}>
-                        {index > 0 && ","}
-                        {genre}
+                        {index > 0 && ", "}
+                        {genre.genreName}
                       </span>
                     ))}
                   </div>
                 </li>
                 <li className="d-flex mb-2">
                   <p className="me-2 fw-bold">Тривалість:</p>
-                  <p>{movie?.duration} min</p>
+                  <p>{movie?.duration}</p>
                 </li>
                 <li className="d-flex">
                   <p className="me-2 fw-bold">Виробництво:</p>
-                  <p>{movie?.production}</p>
+                  <p>
+                    {movie?.producingCountries
+                      ?.map((country) => country.name)
+                      .join(", ")}
+                  </p>
+                </li>
+                <li className="d-flex">
+                  <p className="me-2 fw-bold">Рейтинг:</p>
+                  <p>
+                  <StarReview rating={5} size={16} />
+                  </p>
                 </li>
               </ul>
 
-              <p className="lead"> {movie?.description}</p>
+              <p className=""> {movie?.description}</p>
             </div>
           </div>
         </div>
         <hr />
       </div>
+
       <div className="container p-0 d-lg-none mt-5">
         <div className="d-flex justify-content-center aligh-items-center">
-          {movie?.imgUrl ? (
-            <img src={movie?.imgUrl} alt={movie?.title} className="rounded" />
+          {movie?.trailerUrl ? (
+            <Video videoUrl={movie?.trailerUrl} />
           ) : (
             <img
-              src="https://via.placeholder.com/250x350"
-              alt="movie poster"
+              src={movie?.imageUrls[0]}
+              alt={movie?.title}
               className="rounded"
             />
           )}
@@ -127,12 +182,26 @@ const MovieCheckout = () => {
         <div className="mt-4">
           <div className="ml-2 text-white text-center">
             <h2>{movie?.title}</h2>
-            <h5 className="text-primary"> {movie?.production}</h5>
+            <p>
+              <StarReview rating={4} size={16} />
+            </p>
+            <h5 className="text-primary">
+              {" "}
+              {movie?.producingCountries
+                ?.map((country) => country.name)
+                .join(", ")}
+            </h5>
             <p className="lead"> {movie?.description}</p>
           </div>
         </div>
         <hr />
       </div>
+
+      {isUserAuthenticated && movie?.videoUrl && (
+        <Video videoUrl={movie?.videoUrl} />
+      )}
+      <CheckoutAndReview />
+      <Reviews movieId={movieId} />
     </div>
   );
 };
