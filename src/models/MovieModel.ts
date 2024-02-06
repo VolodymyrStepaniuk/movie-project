7d3d90ff9f03b33ca3aba280c8f1ad3f057cbd15
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-02-06 14:33:15 +0200

    feat(movie): Changed Movie model

--- src/models/MovieModel.ts
+++ src/models/MovieModel.ts
@@ -1,87 +1,64 @@
-class Movie{
-    constructor(private _id:number,private _title: string,private _year: number,
-        private _production: string,private _genres: string[],private _director: string,
-        private _duration: number,private _language: string,
-        private _description:string,private _imgUrl:string){
+import Country from "./CountryModel";
+import Director from "./DirectorModel";
+import Genre from "./GenreModel";
 
-    }
+class Movie{
+    constructor(private _id:number, private _title:string, private _requiredAge:number, private _year:number,
+        private _producingCountries: Country[], private _genres: Genre[], private _directors: Director[],
+        private _duration: string, private _language:string, private _description:string, private _trailerUrl:string,
+        private _videoUrl:string, private _imageUrls:string[])
+        {}
+
     public get id(): number {
         return this._id;
-    }
-
-    public set id(id: number) {
-        this._id = id;
     }
 
     public get title(): string {
         return this._title;
     }
 
-    public set title(title: string) {
-        this._title = title;
+    public get requiredAge(): number {
+        return this._requiredAge;
     }
 
     public get year(): number {
         return this._year;
     }
 
-    public set year(year: number) {
-        this._year = year;
+    public get producingCountries(): Country[] {
+        return this._producingCountries;
     }
 
-    public get production(): string {
-        return this._production;
-    }
-
-    public set production(production: string) {
-        this._production = production;
-    }
-
-    public get genres(): string[] {
+    public get genres(): Genre[] {
         return this._genres;
     }
 
-    public set genres(genres: string[]) {
-        this._genres = genres;
+    public get directors(): Director[] {
+        return this._directors;
     }
 
-    public get director(): string {
-        return this._director;
-    }
-
-    public set director(director: string) {
-        this._director = director;
-    }
-
-    public get duration(): number {
+    public get duration(): string {
         return this._duration;
-    }
-
-    public set duration(duration: number) {
-        this._duration = duration;
     }
 
     public get language(): string {
         return this._language;
     }
 
-    public set language(language: string) {
-        this._language = language;
-    }
-
-    public get imgUrl():string{
-        return this._imgUrl;
-    }
-
-    public set imgUrl(imgUrl:string){
-        this._imgUrl = imgUrl;
-    }
-    public get description():string{
+    public get description(): string {
         return this._description;
     }
 
-    public set description(description:string){
-        this._description = description;
+    public get trailerUrl(): string {
+        return this._trailerUrl;
+    }
+
+    public get videoUrl(): string {
+        return this._videoUrl;
+    }
+
+    public get imageUrls(): string[] {
+        return this._imageUrls;
     }
 }
 
